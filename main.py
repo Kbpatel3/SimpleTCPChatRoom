d4e73a55735087d70ccbd94f01fae16a1487a22c
kbpatel <kbpatel3@catamount.wcu.edu>
2023-08-31 23:27:25 -0400

    Added public and private key to allow encryption of messages.

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import socket
 import threading
 import rsa
 
+# Generate public and private keys
+public_key, private_key = rsa.newkeys(1024)
+public_client_key = None
+
+
 connection_type = input("Press (1) to host a server, (2) to connect to a server: ")
 
 if connection_type == "1":
@@ -13,10 +18,22 @@ if connection_type == "1":
     client_socket, _ = server_socket.accept()
     print("Client connected.")
 
+    # Send public key to client
+    client_socket.send(public_key.save_pkcs1('PEM'))
+
+    # Receive client's public key
+    public_client_key = rsa.PublicKey.load_pkcs1(client_socket.recv(1024))
+
 elif connection_type == "2":
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(("100.64.15.30", 2000))
     print("Connected to server.")
+
+    # Receive server's public key
+    public_client_key = rsa.PublicKey.load_pkcs1(client_socket.recv(1024))
+
+    # Send public key to server
+    client_socket.send(public_key.save_pkcs1('PEM'))
 else:
     print("Invalid input.")
     exit()
@@ -25,13 +42,13 @@ else:
 def send_message(client):
     while True:
         message = input("")
-        client.send(message.encode())
+        client.send(rsa.encrypt(message.encode(), public_client_key))
         print("You: " + message)
 
 
 def receive_message(client):
     while True:
-        print("Client: " + client.recv(1024).decode())
+        print("Client: " + rsa.decrypt(client.recv(1024), private_key).decode())
 
 
 threading.Thread(target=send_message, args=(client_socket,)).start()
